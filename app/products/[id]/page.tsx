bb18da96f21484e2184dc8bae1fa200c0e60522d
ahad <engrabdulahad924@gmail.com>
2026-02-27 16:06:55 +0500

    [fix]: fixed bg issue

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -74,7 +74,7 @@ export default function ProductDetailPage() {
 
 
   return (
-    <div className="w-full px-4 md:px-12 py-8 max-w-7xl mx-auto">
+    <div className="w-full px-4 md:px-12 py-8 max-w-7xl mx-auto bg-white">
       {/* Product Details Section */}
       <div className="flex flex-col md:flex-row gap-8 md:gap-12 mb-12">
         {/* Product Image */}
